ee1e175c128f06c47a259d55be78d860658b5ffd
Terwer <youweics@sina.com>
2019-04-11 12:22:44 +0800

    vnc server black screen fedora29

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,17 @@ CURDIR=/home/$USER
 cd $CURDIR
 echo "Change workspace to $CURDIR=>" && pwd && whoami
 
+# 安装工作套件
+dnf install @developer-workstation-environment -y
+
 # 关闭VNC
+chmod 777 /tmp
+rm /tmp/.X1-lock
 if [ -d ~/.vnc ]; then
-  vncserver -kill :1
+ vncserver -kill :1
 fi
 # 启动VNCServer
-vncserver
+vncserver :1 -geometry 800x600 -depth 24 -SecurityTypes None 
 
 echo "finished."
 bash
