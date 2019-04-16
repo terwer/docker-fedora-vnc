83e5eed1615a0d1b1e5d96ef1ab711e2326ec198
terwer <cbgtyw@gmail.com>
2019-04-16 12:35:05 +0800

    Simplify script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,5 @@
-# 修改工作空间
-CURDIR=/home/$USER
-cd $CURDIR
-echo "Change workspace to $CURDIR=>" && pwd && whoami
-
-# 安装工作套件
-# dnf install @developer-workstation-environment -y
-
-# 关闭VNC
-chmod 777 /tmp
-rm /tmp/.X1-lock
-if [ -d ~/.vnc ]; then
- vncserver -kill :1
-fi
 # 启动VNCServer
-vncserver :1 -geometry 800x600 -depth 24 -SecurityTypes None 
+# vncserver :1 -geometry 800x600 -depth 24 -SecurityTypes None 
 
 echo "finished."
 bash
