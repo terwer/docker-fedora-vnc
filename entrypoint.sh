e881de49dad216b62fa54ebbd2d7ad01d5f86364
terwer <cbgtyw@gmail.com>
2019-04-16 22:03:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # vncserver :1 -geometry 800x600 -depth 24 -SecurityTypes None 
 
 # 安装zsh
-# yum install zsh -y
+# sudo dnf install zsh -y
 # sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # vim ~/.zshrc
